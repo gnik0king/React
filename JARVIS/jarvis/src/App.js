3823f237a95f37fa02b7a7dbe0648e6550380b6f
Thomas Wangia <tuwangia@gmail.com>
2024-11-23 23:05:56 -0800

    Files moved and working project

--- JARVIS/jarvis/src/App.js
+++ JARVIS/jarvis/src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
-import Header from "./components/Header";
-import TaskInput from "./components/TaskInput";
-import TaskList from "./components/TaskList";
-import ProgressBar from "./components/ProgressBar";
+import Header from './components/Header';
+import TaskInput from './components/TaskInput';
+import TaskList from './components/TaskList';
+import ProgressBar from './components/ProgressBar';
 
 function App() {
   const [tasks, setTasks] = useState([]);
